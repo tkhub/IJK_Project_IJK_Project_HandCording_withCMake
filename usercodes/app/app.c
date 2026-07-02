cf033b8c6c2746d06f308cf748b022cb999f7d80
MiNaporitan <mizuki_yamazaki@mail.toyota.co.jp>
2026-07-02 23:34:18 +0900

    ベーススピード0.25；KP曲がり補正値0.01

--- usercodes/app/app.c
+++ usercodes/app/app.c
@@ -11,6 +11,8 @@
 
 /*========VVVV Include Local Header START VVVV===============================*/
 #include "app.h"
+#include <sac.h>
+#include <math.h>
 /*========AAAA Include Local Header END AAAA=================================*/
 
 /*========VVVV Typedef Definition START VVVV=================================*/
@@ -39,18 +41,20 @@
 /**
  * @brief マクロ MAX_NANK の説明
  */
-/* #define MAX_NANKA 256 */
+ /*#define MAX_NANKA 256 */
+ #define MOTOR_BASE_SPEED  (0.25f)  /* モータの基本速度 */
+ #define MOTOR_KP          (0.01f) /* 比例制御のゲイン */
 /*========AAAA MACRO Definition END AAAA=====================================*/
 
 /*========VVVV GLOBAL Variable Definition START VVVV=========================*/
-/* int global_var; */ /* ヘッダファイルで説明済み */
+int global_var; */ /* ヘッダファイルで説明済み */
 /*========AAAA GLOBAL Variable Definition END AAAA===========================*/
 
 /*========VVVV Private Variable Definition START VVVV========================*/
 /**
  * @brief 内部グローバル変数 internal_var の説明
  */
-/* static int internal_var; */
+/*static int internal_var; */
 /*========AAAA Private Variable Definition END AAAA==========================*/
 
 /*========VVVV Private Function Prototype Declaration START VVVV=============*/
@@ -61,33 +65,79 @@
  * @return 戻り値の説明
  * @details 詳細な説明
  */
-/* static int internalTestFunc(int* buffer, int size); */
+static int internalTestFunc(int* buffer, int size); 
+
 /*========AAAA Private Function Prototype Declaration END AAAA===============*/
 
+static int run_flag; /* ライントレーサーの動作フラグ */
+
 /*========VVVV GLOBAL Function Definition START VVVV=========================*/
-/*
- * int testFunc(int* buffer, int size) {
+
+int testFunc(int* buffer, int size) {
  *
  * }
-*/
+
 /** APPの初期化関数 */
 void appInit(void) {
+    run_flag = 0;
+
+    /* モーター停止*/
+    motorsDrive(0.0f, 0.0f);
 
 }
 
 /** APPの1msインターバルハンドラ */
 void appIntervalHandler_1ms(void) {
-    
+    float lineread;
+    float motor_speed;
+    float motorL;
+    float motorR;
+
+    /*停止中*/
+    if(run_flag == 0){
+        motorsDrive(0.0f, 0.0f);
+        return;
+    }
+
+    /*ラインからのズレをmm単位で計測する関数*/
+    lineread = linesensorReadPosition();
+
+    /*API仕様*/
+    /*(左にズレたら- 右に+　中央0)*/
+    motor_speed = lineread * MOTOR_KP;
+
+    /*左旋回*/
+    motorL = MOTOR_BASE_SPEED - motor_speed;
+    /*右旋回*/
+    motorR = MOTOR_BASE_SPEED + motor_speed;
+    motorsDrive(motorL, motorR);
+
 }
 
 /** APPの10msインターバルハンドラ */
 void appIntervalHandler_10ms(void) {
+    /*特に使用しない*/
 
 }
 
 /** APPのメインループ */
 void appMainLoop(void){
+    uiswevent_t sw_status;
+    /*スイッチの状態を取得する関数*/
+    sw_status = uiswRead();
 
+    /*スイッチが押されたら動作開始*/
+    if(sw_status == UISW1_PUSH_EVENT){
+        run_flag = !run_flag;
+
+        if(run_flag){
+            buzzerSetScheduleMs(100,0);
+        }
+        else{
+            motorsDrive(0.0f, 0.0f);
+            buzzerSetScheduleMs(50,0);
+        }
+    }
 }
 
 /*========AAAA GLOBAL Function Definition END AAAA===========================*/
